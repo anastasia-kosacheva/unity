42790933775e7d81fd295140b23909e1f784db2c
Anastasia Kosacheva <anastasia.kosacheva@icloud.com>
2023-02-06 10:04:16 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+/* This declaration is a bit different to what we do in Unity,
+but necessary in order to run this live snippet
+*/
+class MainClass {
+    static void Main() {
+        
+        // show the result in the console
+        // in Unity we use "print()" or "Debug.Log()"
+        Console.WriteLine("Hello");   
+     
+    }
+}
